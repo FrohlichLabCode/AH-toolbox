f0e542520c41b28ca500a9509406cbb742d63069
angelvv <38871824+angelvv@users.noreply.github.com>
2019-08-06 13:48:48 -0400

    Create getCSRTTInfo.m

---
+++ getCSRTTInfo.m
@@ -0,0 +1,18 @@
+function [alignNames, delayNames, delayTypes, hitMissNames, optoNames] = getCSRTTInfo(level)
+
+delayNames   = {'D4','D5','D6','Dall'};
+delayTypes   = {4,5,6,'Dall'};
+optoNames    = {'Theta', 'Alpha', 'ArTheta','ArAlpha','Sham','Oall'};
+hitMissNames = {'Correct','Premature','Incorrect','Omission','noPremature'};
+% Trial inforamtion
+if level(1) == '6'
+    alignNames = {'Init','Stim','Touch'};
+    if length(level)>1 && level(2) == 'a'
+        delayNames = {'D2'};
+        delayTypes = {2};
+    end
+elseif level(1) == '7'
+    alignNames = {'Init','Stim','Touch','Opto'};
+    optoNames  = {'Theta','Alpha','ArTheta','ArAlpha','Sham','Oall'};
+end
+end
